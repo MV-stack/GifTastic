47f321b5ff68c5b98057477b6b2d6cf4ea99c272
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-03-11 15:15:27 -0700

    added rating to giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,12 @@
 var topics = ['wolves', 'offroad', 'homesteading', 'alaska', 'fishing', 'fitness'];
 
  function genButtons(){
+
+    $("#buttons-view").empty();
+    
     for(i = 0; i <topics.length; i++) {
        var button =  $('<button/>', {
-           text: topics[i], //set text 1 to 10
+           text: topics[i], 
            class: "top-genButtons",
            
        });
@@ -17,31 +20,39 @@ var topics = ['wolves', 'offroad', 'homesteading', 'alaska', 'fishing', 'fitness
  $(document).on("click", ".top-genButtons", function(){
      var title = $(this).attr("data-value");
      console.log("title: "+title)
-     var apikey ="Pi3IpyjBiZFkZmSASKn4J57JdmSj6rlf"
-     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +title +"&api_key=Pi3IpyjBiZFkZmSASKn4J57JdmSj6rlf&limit=10"
+     var apiKey = "Pi3IpyjBiZFkZmSASKn4J57JdmSj6rlf";
+     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +title +"&api_key="+apiKey+"&limit=10&rating<=pg"
 
-        // Creates AJAX call for the specific movie button being clicked
+        // Create AJAX call for the specific topic button being clicked
         $.ajax({
           url: queryURL,
           method: "GET"
-        }).then(function(response) {
-
-          // YOUR CODE GOES HERE!!!
-          console.log(response.data)
+        }).then(function(response) {          
+          // Stores giphy from topic button
           var results = response.data;
+          console.log(results);
+          // Storing the rating data
+          var rating = response.Rated;
+          // Creating an element to have the rating displayed
+          //var pOne = $("<p>").text("Rating: " + rating);
+          // Displaying the rating
+          //results.append(pOne);
+
+          // for loop to create div to hold giphy
           for (var i =0; i < results.length; i++) {
               console.log(i)
-              var giphyCard = $("<div>")
-          
+              var giphyCard = $("<div>")                        
               var animatedURL = results[i].images.fixed_height.url;
               var stillURL  = results[i].images.fixed_height_still.url;
-
               var giphyImage = $("<img>");
+              var giphyRating = ("<p>Rating: " + results[i].rating.toUpperCase() + "</p>");
               giphyImage.attr("src",stillURL );
+              giphyCard.append(giphyRating)
+              
           
               giphyImage.attr("data-state", "still");
-              giphyImage.addClass("gify-img");
-                console.log(giphyImage)
+              giphyImage.addClass("giphy-img");
+                
               giphyCard.append(giphyImage)
               $(".giphy-section").append(giphyCard);
           }
